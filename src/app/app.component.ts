0e21b107a319762af8450aaa3bb77435fccd98c8
jungun.park <jungun.park@makeus.com>
2016-09-22 06:54:00 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { WikipediaSearchService } from './wikipedia-search.service'
+
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WikipediaSearchService]
 })
 export class AppComponent {
   title = 'app works!';
+
+  items : Array<String>
+  constructor(private service:WikipediaSearchService) {}
+
+  search(term:string) {
+	this.service.search(term).subscribe(results => this.items = results);
+  }
 }
